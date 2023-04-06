47538769cfddc918cdc66848c7f8992f5bab7cce
Oliver Gut <oliver.gut@flenny.net>
2023-04-06 22:21:41 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+[![Languages](https://skillicons.dev/icons?i=js,html,css,gcp,bash,githubactions,linux,vscode)](https://skillicons.dev)
+
+[![Deploy](https://github.com/flenny/ferienpass-seeberg/actions/workflows/deploy.yml/badge.svg)](https://github.com/flenny/ferienpass-seeberg/actions/workflows/deploy.yml)
+![GitHub](https://img.shields.io/github/license/flenny/ferienpass-seeberg)
+
+# Ferienpass Seeberg Registration Form
+
+Welcome to the ferienpass-seeberg repository! This repository contains the source code and documentation for the registration form of the Ferienpass Seeberg, a holiday program for kids in the Seeberg region of Switzerland.
+
+## About the Ferienpass Seeberg
+
+The Ferienpass Seeberg is a popular program for children between the ages of 6 and 16, organized by the association Ferienpass Seeberg. The program offers a variety of activities and events during one week in the summer holidays, such as sports, crafts, excursions, and much more. Children can choose from a wide range of activities and have fun with other kids while learning new things.
+
+## About the Registration Form
+
+The registration form is primarily designed to allow parents to book courses for their children. The website is built using HTML, CSS, and JavaScript and uses Google Apps Script as a backend for processing the course bookings. The registration form includes the following features:
+
+- Information about the program, including dates, activities, and prices
+- Detailed information about each course, including availability and age requirements
+- A registration form for parents to book courses for their children
+- An administration interface (Google Spreadsheet) to manage the courses, registrations and volunteers
+
+## Contributing
+
+We welcome contributions to the ferienpass-seeberg repository! If you find a bug or have a feature request, please open an issue on the repository.
+
+## License
+
+The ferienpass-seeberg repository is released under the MIT License. See the LICENSE file for details.
+
+## Contact
+
+If you have any questions or comments about the Ferienpass Seeberg or the website, please contact us at support@ferienpass-seeberg.ch.
---
+++ src/Server.js
@@ -0,0 +1,270 @@
+const BOOKINGS_SHEET_NAME = 'Bookings'
+const EVENTS_SHEET_NAME = 'Events'
+const VOLUNTEERS_SHEET_NAME = 'Volunteers'
+const SCRIPT_LOCK_TIMEOUT = 40000
+const EVENT_CACHE_IDENTIFIER = 'event-cache'
+const BOOKINGS_CACHE_IDENTIFIER = 'bookings-cache'
+const CACHE_EXPIRATION_TIMEOUT = 360
+const TIMESTAMP_FORMAT = 'dd.MM.yyyy HH:mm:ss'
+const TIME_ZONE = 'Europe/Zurich'
+const EVENTS_SHEET_ID_COLUMN = 'A:A'
+const EVENTS_SHEET_NAME_COLUMN = 'B:B'
+const EVENTS_SHEET_COSTS_COLUMN = 'E:E'
+const EVENTS_SHEET_ADDITIONAL_COSTS_COLUMN = 'F:F'
+const EVENTS_SHEET_PARTICIPANT_LIMIT_COLUMN = 'M:M'
+const BOOKINGS_SHEET_EVENT_ID_COLUMN = 'L'
+
+/**
+ * Sets up the script properties, which are used to store the spreadsheet id and the email addresses.
+ * This function needs to be executed only once, when the script is first installed.
+ * */
+function setupScriptProperties() {
+  PropertiesService.getScriptProperties().setProperties({
+    SPREADSHEET_ID: SpreadsheetApp.getActiveSpreadsheet().getId(),
+    EMAIL_FROM_NAME: 'John Doe',
+    EMAIL_FROM_ADDRESS: 'john.doe@example.com',
+    EMAIL_SUPPORT_REQUESTS: 'support@example.com',
+  }, true)
+}
+
+/** Gets the index.html page for every HTTP GET reguest. */
+function doGet(request) {
+  const template = HtmlService.createTemplateFromFile(request.parameter?.action ?? 'index')
+  template.data = {
+    reference: request.parameter?.reference,
+    bookingId: request.parameter?.bookingId,
+    origin: request.parameter?.origin ?? ScriptApp.getService().getUrl(),
+  }
+  return template.evaluate()
+    .addMetaTag('viewport', 'width=device-width, initial-scale=1')
+    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
+}
+
+/** Gets the content for the specified @filename. */
+function include(filename) { return HtmlService.createHtmlOutputFromFile(filename).getContent() }
+
+/** Gets all the events including updated availability information. */
+function getEvents() {
+  const result = CacheService.getScriptCache().get(EVENT_CACHE_IDENTIFIER) ??
+    updateCache(EVENT_CACHE_IDENTIFIER, EVENTS_SHEET_NAME)
+  return Object.values(JSON.parse(result))
+}
+
+/** Gets the bookings for the specified reference. */
+function getBookings(reference) {
+  const result = CacheService.getScriptCache().get(BOOKINGS_CACHE_IDENTIFIER) ??
+    updateCache(BOOKINGS_CACHE_IDENTIFIER, BOOKINGS_SHEET_NAME)
+  return Object.values(JSON.parse(result))
+    .filter(booking => booking?.find(entry => entry === reference))
+}
+
+/** Updates all caches (event- and booking cache). */
+function updateCaches() {
+  updateCache(EVENT_CACHE_IDENTIFIER, EVENTS_SHEET_NAME)
+  updateCache(BOOKINGS_CACHE_IDENTIFIER, BOOKINGS_SHEET_NAME)
+}
+
+function processForm(formObject) {
+  const lock = LockService.getScriptLock()
+  if (!lock.tryLock(SCRIPT_LOCK_TIMEOUT)) throw new Error('Could not obtain lock after 40 seconds.')
+
+  try {
+    const spreadsheet = SpreadsheetApp.openById(
+      PropertiesService.getScriptProperties().getProperty('SPREADSHEET_ID')
+    )
+
+    const bookingsSheet = spreadsheet.getSheetByName(BOOKINGS_SHEET_NAME)
+    const bookingsSheetHeaders = bookingsSheet.getRange(1, 1, 1, bookingsSheet.getLastColumn()).getValues()[0]
+    const bookingsSheetNextRow = bookingsSheet.getLastRow() + 1
+
+    const timestamp = Utilities.formatDate(new Date(), TIME_ZONE, TIMESTAMP_FORMAT)
+    const bookingId = createSignature(Date.now(), PropertiesService.getScriptProperties().getProperty('SPREADSHEET_ID'), 6)
+    const reference = createSignature(
+      formObject.find(entry => entry[0] === 'email')?.[1],
+      PropertiesService.getScriptProperties().getProperty('SPREADSHEET_ID'))
+
+    const origin = formObject.find(entry => entry[0] === 'origin')?.[1]
+    const phone = `'${formObject.find(entry => entry[0] === 'phone')?.[1]}`.trim()
+    let bookings = []
+
+    // create a full record for every booked event
+    formObject.filter(param => param[0] === 'eventId').forEach(param => {
+      const [, eventId] = param
+      bookings.push(bookingsSheetHeaders.map((header) => {
+        if (header === 'timestamp') return timestamp
+        else if (header === 'id') return bookingId
+        else if (header === 'eventId') return eventId
+        else if (header === 'phone') return phone
+        else if (header === 'grade') return `${formObject.find(entry => entry[0] === 'grade')?.[1]}. Klasse`
+        else if (header === 'eventName') return `=XLOOKUP(INDIRECT(CONCATENATE("${BOOKINGS_SHEET_EVENT_ID_COLUMN}",ROW())),${EVENTS_SHEET_NAME}!${EVENTS_SHEET_ID_COLUMN},${EVENTS_SHEET_NAME}!${EVENTS_SHEET_NAME_COLUMN},"")`
+        else if (header === 'waitingList') return `=IF((XLOOKUP(INDIRECT(CONCATENATE("${BOOKINGS_SHEET_EVENT_ID_COLUMN}",ROW())),${EVENTS_SHEET_NAME}!${EVENTS_SHEET_ID_COLUMN},${EVENTS_SHEET_NAME}!${EVENTS_SHEET_PARTICIPANT_LIMIT_COLUMN},""))-(COUNTIF(${BOOKINGS_SHEET_EVENT_ID_COLUMN}2:INDIRECT(CONCATENATE("${BOOKINGS_SHEET_EVENT_ID_COLUMN}",ROW())),INDIRECT(CONCATENATE("${BOOKINGS_SHEET_EVENT_ID_COLUMN}",ROW())))) < 0, "Ja", "Nein")`
+        else if (header === 'costs') return `=XLOOKUP(INDIRECT(CONCATENATE("${BOOKINGS_SHEET_EVENT_ID_COLUMN}",ROW())),${EVENTS_SHEET_NAME}!${EVENTS_SHEET_ID_COLUMN},${EVENTS_SHEET_NAME}!${EVENTS_SHEET_COSTS_COLUMN},"")`
+        else if (header === 'additionalCosts') return `=XLOOKUP(INDIRECT(CONCATENATE("${BOOKINGS_SHEET_EVENT_ID_COLUMN}",ROW())),${EVENTS_SHEET_NAME}!${EVENTS_SHEET_ID_COLUMN},${EVENTS_SHEET_NAME}!${EVENTS_SHEET_ADDITIONAL_COSTS_COLUMN},"")`
+        else if (header === 'reference') return reference
+        else if (header === 'url') return getStatusUrl(origin, reference)
+        else return formObject.find(entry => entry[0] === header)?.[1]
+      }))
+    })
+
+    bookingsSheet.getRange(bookingsSheetNextRow, 1, bookings.length, bookingsSheetHeaders.length).setValues(bookings)
+
+    // add optional volunteers
+    const volunteeringId = formObject.find(entry => entry[0] === 'volunteering')?.[1]
+    if (volunteeringId) {
+      const volunteersSheet = spreadsheet.getSheetByName(VOLUNTEERS_SHEET_NAME)
+      const volunteersSheetHeaders = volunteersSheet.getRange(1, 1, 1, volunteersSheet.getLastColumn()).getValues()[0]
+      const volunteersSheetNextRow = volunteersSheet.getLastRow() + 1
+
+      volunteersSheet.getRange(volunteersSheetNextRow, 1, 1, volunteersSheetHeaders.length).setValues(
+        [volunteersSheetHeaders.map((header) => {
+          if (header === 'phone') return phone
+          else if (header === 'option') return volunteeringId
+          else if (header === 'volunteering') return `=LOOKUP(INDIRECT(CONCATENATE("B",ROW())),Volunteering!A:A,Volunteering!B:B)`
+          else return formObject.find(entry => entry[0] === header)?.[1]
+        })]
+      )
+    }
+
+    // update caches
+    CacheService.getScriptCache().put(EVENT_CACHE_IDENTIFIER, getDataFromSheet(spreadsheet, EVENTS_SHEET_NAME), CACHE_EXPIRATION_TIMEOUT)
+    CacheService.getScriptCache().put(BOOKINGS_CACHE_IDENTIFIER, getDataFromSheet(spreadsheet, BOOKINGS_SHEET_NAME), CACHE_EXPIRATION_TIMEOUT)
+
+    // optional machen, oder try catch
+    // unhandled exceptions per e-mail
+    const emailTo = formObject.find(entry => entry[0] === 'email')?.[1]
+    const firstName = formObject.find(entry => entry[0] === 'firstName')?.[1]
+    const htmlBody = `
+    <html>
+      <body style="font-size: 16px;">
+        <img src="https://booking.ferienpass-seeberg.ch/ferienpass.webp" alt="Logo Ferienpass Seeberg" style="max-width: 12em;">
+        <p style="font-size: 20px;">Hallo ${firstName} 👋🏻<p>
+        <p>Vielen herzlichen Dank für deine Anmeldung beim Ferienpass Seeberg. Du kannst
+        <a href="${getStatusUrl(origin, reference)}">hier</a> jederzeit
+        den Status deiner gebuchten Kurse überprüfen. Die Rechnung, unter Berücksichtigung
+        der im Programmheft beschriebenen Familienpauschale, erhältst du nach Anmeldeschluss.</p>
+        <p><a href="${getPreFilledFormUrl(origin, bookingId)}" target="_top">Hier</a> kannst du weitere Kurse buchen.</p>
+        <p>Tschüss und bis bald</p>
+        <p>Dein Ferienpass Seeberg Team</p>
+      </body>
+    </html>
+    `
+
+    const textBody = `
+      Hallo ${firstName}
+
+      Vielen herzlichen Dank für deine Anmeldung beim Ferienpass Seeberg. Unter dem nachfolgenden
+      Link kannst du jederzeit den Status deiner gebuchten Kurse ueberpruefen.
+
+      ${getStatusUrl(origin, reference)}
+
+      Die Rechnung, unter Beruecksichtigung der im Programmheft beschriebenen Familienpauschale,
+      erhaeltst du nach Anmeldeschluss.
+
+      Hier kannst du weitere Kurse buchen.
+
+      ${getPreFilledFormUrl(origin, bookingId)}
+
+      Tschuess und bis bald.
+      Dein Ferienpass Seeberg Team
+    `
+    sendMail({
+      to: emailTo,
+      emailFrom: PropertiesService.getScriptProperties().getProperty('EMAIL_FROM_ADDRESS'),
+      nameFrom: PropertiesService.getScriptProperties().getProperty('EMAIL_FROM_NAME'),
+      subject: "Deine Anmeldung beim Ferienpass Seeberg 🎉",
+      textBody: textBody,
+      htmlBody: htmlBody,
+    })
+
+    // return redirect url
+    return `${origin}?action=success&bookingId=${bookingId}`
+  }
+  catch (error) {
+    errorMsg = `${error}<br>${error.stack}<br><p>RequestData:</p><code>${formObject}</code>`
+    sendMail({
+      to: PropertiesService.getScriptProperties().getProperty('EMAIL_SUPPORT_REQUESTS'),
+      emailFrom: PropertiesService.getScriptProperties().getProperty('EMAIL_FROM_ADDRESS'),
+      nameFrom: PropertiesService.getScriptProperties().getProperty('EMAIL_FROM_NAME'),
+      subject: "Fehler im Ferienpass Backend 🙈",
+      textBody: errorMsg,
+      htmlBody: errorMsg,
+    })
+
+    throw error
+  }
+  finally { lock.releaseLock() }
+}
+
+const getDataFromSheet = (spreadsheet, sheetName) => {
+  const sheet = spreadsheet.getSheetByName(sheetName)
+  let numRows = sheet.getLastRow() - 1
+  if (numRows === 0) numRows = 1
+  return JSON.stringify(
+    sheet.getRange(2, 1, numRows, sheet.getLastColumn()).getValues()
+  )
+}
+
+/** Creates a custom signature for the given value and key. */
+const createSignature = (value, key, length) => {
+  const signature = Utilities.computeHmacSignature(
+    Utilities.MacAlgorithm.HMAC_MD5,
+    value,
+    key,
+    Utilities.Charset.US_ASCII);
+  return Utilities.base64EncodeWebSafe(signature)
+    .replace(/[_\-=]+/g, '')
+    .substring(0, length ?? 12)
+}
+
+const updateCache = (identifier, sheetName) => {
+  let result
+  const lock = LockService.getScriptLock()
+  const hasLock = lock.tryLock(SCRIPT_LOCK_TIMEOUT)
+  if (!hasLock) { throw new Error('Could not obtain lock after 40 seconds.') }
+  try {
+    const spreadsheet = SpreadsheetApp.openById(
+      PropertiesService.getScriptProperties().getProperty('SPREADSHEET_ID'))
+    const data = getDataFromSheet(spreadsheet, sheetName)
+    CacheService.getScriptCache().put(identifier, data, CACHE_EXPIRATION_TIMEOUT)
+    result = data
+  }
+  catch (error) { throw error }
+  finally { lock.releaseLock() }
+  return result
+}
+
+// Get the status url
+const getStatusUrl = (baseUrl, reference) => `${baseUrl}?action=status&reference=${reference}`
+
+// Get the pre-filled form url
+const getPreFilledFormUrl = (baseUrl, bookingId) => `${baseUrl}?bookingId=${bookingId}`
+
+// Send an email
+const sendMail = email => Gmail.Users.Messages.send({ raw: convertToGmailMessage(email) }, "me");
+const convertToGmailMessage = ({ to, emailFrom, nameFrom, subject, textBody, htmlBody }) => {
+  const boundary = "boundaryboundary";
+  const mailData = [
+    `MIME-Version: 1.0`,
+    `To: ${to}`,
+    nameFrom && emailFrom ? `From: "${nameFrom}" <${emailFrom}>` : "",
+    `Subject: =?UTF-8?B?${Utilities.base64Encode(
+      subject,
+      Utilities.Charset.UTF_8
+    )}?=`,
+    `Content-Type: multipart/alternative; boundary=${boundary}`,
+    ``,
+    `--${boundary}`,
+    `Content-Type: text/plain; charset=UTF-8`,
+    ``,
+    textBody,
+    ``,
+    `--${boundary}`,
+    `Content-Type: text/html; charset=UTF-8`,
+    `Content-Transfer-Encoding: base64`,
+    ``,
+    Utilities.base64Encode(htmlBody, Utilities.Charset.UTF_8),
+    ``,
+    `--${boundary}--`,
+  ].join("\r\n");
+  return Utilities.base64EncodeWebSafe(mailData);
+};
